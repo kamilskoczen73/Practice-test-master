887829e79d319500db5bac12d326dec27df17f9d
kamilskoczen73 <59886003+kamilskoczen73@users.noreply.github.com>
2020-08-25 10:18:43 +0200

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,16 @@
+// var sliders =[];
+
+// $(document).ready(function($){
+    
+//     sliders = $( ".slider" ).toArray();
+//     var i=1;
+//     var j=sliders.length;
+
+//     for(i;j;i++){
+//         $( ".slider" ).removeClass( "active" );
+//         $( ".slider" + i ).addClass("active");
+//     }
+//     console.log(sliders);
+//     console.log(j);
+    
+//   });
